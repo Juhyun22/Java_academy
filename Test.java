2bcc9c268c0f57ae77329d2bc9e700b879b789ab
Juhyun22 <45312398+Juhyun22@users.noreply.github.com>
2021-11-10 15:01:53 +0900

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,29 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.print("Hello world!!! ");
+		System.out.print("안녕하세요!!! ");
+		
+		// 주석: 프로그램이 해석하지 않는 문장
+		// 한줄 주석
+		/*
+		 * 여러줄
+		 * 주석
+		 * 입니당.
+		 * */
+		System.out.print("줄바꿈을 하고 싶으면 \n\n\n"); // \n: 줄바꿈
+		System.out.print("줄바꿈이 되었나?");
+		System.out.println("줄바꿈이 된다.");
+		System.out.println("줄바꿈이 되었나?");
+		System.out.println("자동완성: 컨트롤+스페이스바");
+	}
+
+}
+
+
+
+
+
+
