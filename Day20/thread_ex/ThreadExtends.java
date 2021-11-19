02929a2a5dc3ca9defeddf60a9d587ebb1330dc4
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2021-11-19 17:00:20 +0900

    Add files via upload

---
+++ Day20/thread_ex/ThreadExtends.java
@@ -0,0 +1,25 @@
+package thread_ex;
+
+public class ThreadExtends extends Thread {
+	private String resource;
+	
+	public ThreadExtends(String resource) {
+		this.resource = resource;
+	}
+	
+	@Override
+	public void run() {
+		// 여기서 구현하는 내용이 쓰레드에서 수행되는 내용
+		
+		// resource 열번 출력
+		for (int i = 0; i < 10; i++) {
+			try {
+				Thread.sleep(500); // 0.5초씩 쉬면서 찍는다.
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.print(this.resource + " ");
+		}
+	}
+}
